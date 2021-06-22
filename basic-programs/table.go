0fa214496c888874309cc78bc143c8c2d2f9e5e0
Kalebu <isaackeinstein@gmail.com>
2021-06-22 22:30:06 +0300

    + multiplication table

---
+++ basic-programs/table.go
@@ -0,0 +1,22 @@
+// Learning concept of loop in go
+
+// Learning concept of loop in go
+
+package main
+
+import "fmt"
+
+func main() {
+	var limit int
+	var number int
+
+	fmt.Println("Generate Multiplication Table")
+	fmt.Println("Enter Target number ")
+	fmt.Scan(&number)
+	fmt.Println("Enter limit of the table ")
+	fmt.Scan(&limit)
+
+	for i := 1; i <= limit; i++ {
+		fmt.Println(number, "x", i, "=", i*number)
+	}
+}
